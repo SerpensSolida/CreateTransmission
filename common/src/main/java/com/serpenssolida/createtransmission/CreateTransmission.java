d4c8dd48168d02c9bdc766028dc1f288f7e94972
SerpensSolida <alex_s96@hotmail.it>
2026-03-06 23:22:52 +0100

    Modified ponder scene, added datagen for ponder.

--- common/src/main/java/com/serpenssolida/createtransmission/CreateTransmission.java
+++ common/src/main/java/com/serpenssolida/createtransmission/CreateTransmission.java
@@ -1,6 +1,7 @@
 package com.serpenssolida.createtransmission;
 
 import com.serpenssolida.createtransmission.content.chain.AbstractTransmissionChainBlock;
+import com.serpenssolida.createtransmission.ponder.CTPonderIndex;
 import com.simibubi.create.api.contraption.BlockMovementChecks;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.minecraft.resources.ResourceLocation;
@@ -28,6 +29,7 @@ public class CreateTransmission
 
         CTBlocks.init();
         CTBlockEntities.init();
+		CTPonderIndex.init();
 
         //Check used when glue is applied to transmission chain.
         BlockMovementChecks.registerAttachedCheck((state, world, pos, direction) ->
